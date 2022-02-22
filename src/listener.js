66ddde6004c70383418f459b3388d94c92c1fbde
Arslanoov <rasularslanoov@gmail.com>
2022-02-22 19:12:36 +0500

    refactor(lib): Removed unused files

--- src/listener.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.listener = void 0;
-function listener(event) {
-    return this[event.type](event);
-}
-exports.listener = listener;
--- src/mount.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.mount = void 0;
-var mount = function (node, target) {
-    target.replaceWith(node);
-    return node;
-};
-exports.mount = mount;
